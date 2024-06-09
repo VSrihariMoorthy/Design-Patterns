fc7bf75a3e43cc5b3d7b76c025e99c57fb9719fd
VSrihariMoorthy <srihari.vijayakumar@ei.study>
2024-06-10 02:11:18 +0530

    Mediator-Design-Pattern

---
+++ Behavioral-Design-Patterns/Mediator-Design-Pattern/MediatorPatternDemo.java
@@ -0,0 +1,18 @@
+public class MediatorPatternDemo {
+    public static void main(String[] args) {
+        ChatMediator mediator = new ChatMediatorImpl();
+
+        User user1 = new UserImpl(mediator, "Alice");
+        User user2 = new UserImpl(mediator, "Bob");
+        User user3 = new UserImpl(mediator, "Charlie");
+        User user4 = new UserImpl(mediator, "David");
+
+        mediator.addUser(user1);
+        mediator.addUser(user2);
+        mediator.addUser(user3);
+        mediator.addUser(user4);
+
+        user1.send("Hello, everyone!");
+        user2.send("Hey, Alice!");
+    }
+}
---
+++ Behavioral-Design-Patterns/Mediator-Design-Pattern/UserImpl.java
@@ -0,0 +1,17 @@
+public class UserImpl extends User {
+
+    public UserImpl(ChatMediator mediator, String name) {
+        super(mediator, name);
+    }
+
+    @Override
+    public void send(String message) {
+        System.out.println(this.name + " sends: " + message);
+        mediator.sendMessage(message, this);
+    }
+
+    @Override
+    public void receive(String message) {
+        System.out.println(this.name + " receives: " + message);
+    }
+}
